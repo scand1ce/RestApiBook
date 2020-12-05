f72d81418f88e09fd256a7f86a0fa4a29ca50396
Serhii_Tomchuk <dice1@ukr.net>
2020-12-06 00:56:01 +0200

    'Chapter6'

--- chapter56/posts/views.py
+++ chapter56/posts/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from .permissions import IsAuthorOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
 
@@ -9,5 +10,6 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
